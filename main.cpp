f62ddc3281f041f210a67a10c827418d9932ec2e
Ali Kashif <alikashif5917@gmail.com>
2025-04-16 15:27:43 +0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <fstream>
 using namespace std;
 
+// Global Variables/Constants
 
-
+const int screenWidth = 1920;
+const int screenHeight = 1080;
 
 
 
@@ -13,5 +15,22 @@ using namespace std;
 
 int main () {
 
-    
+    InitWindow(screenWidth, screenHeight, "CapTale");
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false) {
+        
+        
+        
+
+        // Drawing
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
